75af661b18ccbb22105692d682b710a699d20a10
Ayushman Bharadwaj <ayushman15899@gmail.com>
2026-09-19 23:12:06 +0530

    fix: Update base to relative path and use peaceiris/actions-gh-pages for reliable deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/plant-health-ar/' : './',
+  base: './',
   plugins: [
     basicSsl(),
   ],
